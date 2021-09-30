fffc868ec4b1c2f01b2475ea4ed2ad9fa25ef6f1
SuryaRajendr <73171944+SuryaRajendr@users.noreply.github.com>
2021-09-30 16:51:18 +0530

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user :'nandy',
+    password :'Surya@123',
+    server:'localhost',
+    database:'nodejs_db',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :'SQLEXPRESS'
+    },
+    port : 51383
+}
+
+module.exports = config; 
